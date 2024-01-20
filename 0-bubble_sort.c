b360b4189b726fd1dc4b003a1de7e94f904257ec
Oluchukwu Ughagwu <oluchristian98@gmail.com>
2024-01-20 02:04:51 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,25 +5,29 @@
  * @array: array to be sorted
  * @size: size of array
  */
- void bubble_sort(int *array, size_t size)
- {
+void bubble_sort(int *array, size_t size)
+{
 	size_t i, j;
 	int swapped, temp;
-	
+
 	if (!array || size < 2)
-        return;
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			temp = array[j];
-			array[j] = array[j+1];
-			array[j+1] = temp;
-			swapped = 1;
-			print_array(array, size);
+			if (temp > array[j + 1])
+			{
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
 		}
 		if (swapped == 0)
 			break;
 	}
- }
+}
+
